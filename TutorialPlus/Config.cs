77cb0c31ab9b013544854614dbf7692577767ea0
Phoenix <thephoenixproject208@gmail.com>
2023-07-12 13:26:45 +0100

    config descriptions

--- TutorialPlus/Config.cs
+++ TutorialPlus/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,16 @@ namespace TutorialPlus
 {
 	public class Config
 	{
+		[Description("Should tutorials be able to trigger SCP-096 and become a target")]
 		public bool TutorialTrigger096 { get; set; } = false;
+		[Description("Should tutorials be able to freeze SCP-173 and become an observer")]
 		public bool TutorialObserve173 { get; set; } = false;
 
+		[Description("Automatically enable godmode when a player spawns as tutorial (Will also automatically disable if a player changes role)")]
 		public bool TutorialGodmode { get; set; } = true;
+		[Description("Automatically enable bypass mode when a player spawns as tutorial (Will also automatically disable if a player changes role)")]
 		public bool TutorialBypass { get; set; } = true;
-		public bool Debug { get; set; }
+		[Description("Enables debug messages in the server console")]
+		public bool Debug { get; set; } = false;
 	}
 }
